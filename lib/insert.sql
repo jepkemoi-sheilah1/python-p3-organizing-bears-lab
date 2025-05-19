d6a6b7ad7ce2cca2e33304fe25b8678b352a9552
Jepkemoi Sheilah <jepkemoishyllah@gmail.com>
2025-05-19 16:54:18 +0300

    completed sql lab

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 5, 'M', 'Brown', 'Calm', 1),
+('Rowdy', 3, 'M', 'Black', 'Aggressive', 1),
+('Tabitha', 2, 'F', 'White', 'Calm', 1),
+('Sergeant Brown', 4, 'M', 'Brown', 'Aggressive', 0),
+('Melissa', 1, 'F', 'Black', 'Calm', 1),
+('Grinch', 6, 'M', 'Brown', 'Aggressive', 0),
+('Wendy', 2, 'F', 'White', 'Calm', 1),
+(NULL, 1, 'F', 'Black', 'Calm', 1);
